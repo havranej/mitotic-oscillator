f467e30e375ec44f6338274e6b7d3ad9d49ca20e
Jan Havránek <j.havranek@gmail.com>
2019-12-23 23:13:19 +0100

    Components generated from a list

--- dash/app.py
+++ dash/app.py
@@ -10,9 +10,28 @@ import numpy as np
 from simulation import Simulation
 from methods import rk4
 
+parameters_list = [
+    'v_i',       
+    'v_d',       
+    'K_d',
+    'k_d',
+    'K_c',
+    'V_M1',
+    'K_1',
+    'V_2',
+    'K_2',
+    'V_M3',
+    'K_3',
+    'V_4',
+    'K_4'
+    ]
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
+
+sliders_list = [dcc.Slider(id = parameter, min = 0, max = 1, step = 0.01, value = 0.05) for parameter in parameters_list]
+
 
 app.layout = html.Div(children = [
     html.H1(children = 'Mitotic oscilator'),
@@ -24,13 +43,7 @@ app.layout = html.Div(children = [
         step = 0.01,
         value = 0.5
     ),
-    dcc.Slider(
-        id = 'v_i',
-        min = 0,
-        max = 1,
-        step = 0.001,
-        value = 0.025
-    ),
+    html.Div(children = sliders_list),
     dcc.Graph(id = 'time-graph', animate = True),
     dcc.Interval(
         id='graph-update',
@@ -39,33 +52,28 @@ app.layout = html.Div(children = [
     ),
 ])
 
-
-
-sim = Simulation(initial_state = np.array([0, 0.01 , 0.01, 0.01]), h = 0.01, logging_frequency = 100, method = rk4)
+input_list = [Input(component_id = parameter, component_property = 'value') for parameter in parameters_list]
 
 @app.callback(
         Output(component_id = 'time-graph', component_property = 'figure'),
-        [Input(component_id = 'graph-update', component_property = 'n_intervals')],
-        state = [State('v_i', 'value')]
+        input_list
+        # state = [State('v_i', 'value')]
 )
-def update_plot(n_intervals, v_i):
-    global sim
+def update_plot(*param_values):
+    sim = Simulation(initial_state = np.array([0, 0.01 , 0.01, 0.01]), h = 0.01, logging_frequency = 100, method = rk4)
+    parameters_dict = {key: parameter for key, parameter in zip(parameters_list, param_values)}
 
-    for i in range(500):
-        sim.step({'v_i': v_i})
+    for i in range(10000):
+        sim.step(parameters_dict)
     
-    LAST_N = 100
-
-    t = sim.get_history('t', last_n = LAST_N)
+    t = sim.get_history('t')
 
     return {
             'data': [
-                {'x': t, 'y': sim.get_history('C', last_n = LAST_N), 'name': 'C'},
-                {'x': t, 'y': sim.get_history('M', last_n = LAST_N), 'name': 'M'},
-                {'x': t, 'y': sim.get_history('X', last_n = LAST_N), 'name': 'X'},
+                {'x': t, 'y': sim.get_history('C'), 'name': 'C'},
+                {'x': t, 'y': sim.get_history('M'), 'name': 'M'},
+                {'x': t, 'y': sim.get_history('X'), 'name': 'X'},
             ],
-            'layout': go.Layout(xaxis=dict(range=[min(t),max(t)]),
-                                yaxis=dict(range=[0,1]),)
         }
 
 
