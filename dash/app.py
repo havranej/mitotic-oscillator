107af147212b580321cf199cc515c426ace5c657
Jan Havránek <j.havranek@gmail.com>
2020-01-13 21:05:49 +0100

    Initial conditions and technical parameters

--- dash/app.py
+++ dash/app.py
@@ -28,19 +28,22 @@ def generate_control_elements(parameters_table):
         param_table_selection = parameters_table[parameters_table.group == group]
 
         for index, row in param_table_selection.iterrows():
-            group_list.append(dcc.Markdown(dangerously_allow_html = True,
-                                                children = row['label']))
+            slider_label = dcc.Markdown(dangerously_allow_html = True,
+                                            children = row['label'])
             marks_dict = {i: str(round(i, ndigits = 4)) for i in np.linspace(row['min'], row['max'], num = 5)}
-            group_list.append(dcc.Slider(id = row['id'],
-                                        min = row['min'],
-                                        max = row['max'],
-                                        step = row['step'],
-                                        value = row['default'],
-                                        marks = marks_dict))
+            slider = dcc.Slider(id = row['id'],
+                                    min = row['min'],
+                                    max = row['max'],
+                                    step = row['step'],
+                                    value = row['default'],
+                                    marks = marks_dict)
+            group_list.append(dbc.Row([dbc.Col(slider_label, width = 1), dbc.Col(slider, width = 10)]))
+            
 
         master_list.append(html.Details(children = group_list))
 
     return master_list
+
 
 app.layout = html.Div(children = [
     html.H1(children = 'Mitotic oscilator'),
@@ -56,32 +59,35 @@ app.layout = html.Div(children = [
     ], fluid = True)
 ])
 
-input_list = [Input(component_id = parameter, component_property = 'value') for parameter in parameters_table['id']]
+input_slider_list = [Input(component_id = parameter, component_property = 'value') for parameter in parameters_table['id']]
 
 @app.callback(
         Output(component_id = 'time-graph', component_property = 'figure'),
-        input_list
-        # state = [State('v_i', 'value')]
+        input_slider_list
 )
 def update_plot(*param_values):
-    sim = Simulation(initial_state = np.array([0, 0.01 , 0.01, 0.01]), h = 0.01, logging_frequency = 100, method = rk4)
     parameters_dict = {key: parameter for key, parameter in zip(parameters_table['id'], param_values)}
 
-    for i in range(10000):
+    step = 10**parameters_dict['h']
+    logging_frequency = 10**parameters_dict['log_f']
+
+    sim = Simulation(initial_state = np.array([0, parameters_dict['C_init'] , parameters_dict['M_init'], parameters_dict['X_init']]), 
+                        h = step, logging_frequency = logging_frequency, method = rk4)
+
+    for i in range(int(100 / step)):
         sim.step(parameters_dict)
     
     t = sim.get_history('t')
 
     return {
             'data': [
-                {'x': t, 'y': sim.get_history('C'), 'name': 'C'},
-                {'x': t, 'y': sim.get_history('M'), 'name': 'M'},
-                {'x': t, 'y': sim.get_history('X'), 'name': 'X'},
+                {'x': t, 'y': sim.get_history('C'), 'name': 'Cyclin'},
+                {'x': t, 'y': sim.get_history('M'), 'name': 'CDC2 kinase'},
+                {'x': t, 'y': sim.get_history('X'), 'name': 'Cyclin protease'},
             ],
             # 'layout': go.Layout(yaxis=dict(range = [0,1]))
             
         }
-
 
 
 if __name__ == '__main__':
